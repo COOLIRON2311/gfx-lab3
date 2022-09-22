ade8ffe422f1e9545826b88b8dce8cbab298139a
SyberiaImperor <Al.B.danilenko@gmail.com>
2022-09-22 21:31:07 +0300

    task1

--- task1/Form1.Designer.cs
+++ task1/Form1.Designer.cs
@@ -1,42 +1,45 @@
-﻿namespace task1;
-
-partial class Form1
+﻿
+namespace lab3
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-        if (disposing && (components != null))
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
         {
-            components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
-        base.Dispose(disposing);
-    }
 
-    #region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(193, 72);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(776, 426);
+            this.pictureBox1.Size = new System.Drawing.Size(663, 397);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
@@ -44,11 +47,33 @@ partial class Form1
             this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 515);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(575, 515);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(242, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load image for floodFill";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1036, 582);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,9 +81,13 @@ partial class Form1
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
-
-    #endregion
-
-    private PictureBox pictureBox1;
 }
+
--- task1/Form1.cs
+++ task1/Form1.cs
@@ -1,90 +1,242 @@
-namespace task1;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace lab3
 {
-    Color col;
-    Bitmap img;
-    Graphics g;
-    Pen pen;
-    bool mouseDown;
-    int x, y;
-    public Form1()
+    public partial class Form1 : Form
     {
-        col = Color.Black;
-        InitializeComponent();
-        pen = new Pen(col, 5);
-        pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
-        img = new Bitmap(Width, Height);
-        g = Graphics.FromImage(img);
-        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-        mouseDown = false;
-    }
 
-    private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
-    {
-        mouseDown = true;
-        x = e.X;
-        y = e.Y;
-    }
-
-    private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
-    {
-        mouseDown = false;
-    }
-
-    private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
-    {
-        if (e.Button == MouseButtons.Right)
+        Color col;
+        Bitmap img;
+        Bitmap tempImg;
+        Graphics g;
+        Pen pen;
+        bool mouseDown;
+        int x, y;
+        public Form1()
         {
-            floodFill(e.X, e.Y);
-            // img.Save("image.png");
-        }
-    }
-
-    private void Form1_KeyDown(object sender, KeyEventArgs e)
-    {
-        if (e.KeyCode == Keys.Escape)
-        {
+            col = Color.Black;
+            InitializeComponent();
+            pen = new Pen(col, 5);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
             img = new Bitmap(Width, Height);
             g = Graphics.FromImage(img);
-            pictureBox1.Image = img;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            mouseDown = false;
         }
-    }
 
-    private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
-    {
-        if (mouseDown && e.Button == MouseButtons.Left)
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            g.DrawLine(pen, new Point(x, y), e.Location);
+            mouseDown = true;
             x = e.X;
             y = e.Y;
-            pictureBox1.Image = img;
         }
-    }
 
-    private void floodFill(int x, int y)
-    {
-        Color targetColor = img.GetPixel(x, y);
-        if (targetColor == col)
-            return;
-        Stack<Point> stack = new Stack<Point>();
-        stack.Push(new Point(x, y));
-        while (stack.Count > 0)
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
-            Point p = stack.Pop();
-            if (img.GetPixel(p.X, p.Y) == targetColor)
+            mouseDown = false;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
             {
-                img.SetPixel(p.X, p.Y, col);
-                if (p.X > 0)
-                    stack.Push(new Point(p.X - 1, p.Y));
-                if (p.X < img.Width - 1)
-                    stack.Push(new Point(p.X + 1, p.Y));
-                if (p.Y > 0)
-                    stack.Push(new Point(p.X, p.Y - 1));
-                if (p.Y < img.Height - 1)
-                    stack.Push(new Point(p.X, p.Y + 1));
+                LinearFloodFillWithImage(e.X, e.Y);
+                // img.Save("image.png");
             }
         }
-        pictureBox1.Image = img;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                img = new Bitmap(Width, Height);
+                g = Graphics.FromImage(img);
+                pictureBox1.Image = img;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown && e.Button == MouseButtons.Left)
+            {
+                g.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+                pictureBox1.Image = img;
+            }
+        }
+
+        private void floodFill(int x, int y)
+        {
+            Color targetColor = img.GetPixel(x, y);
+            if (targetColor == col)
+                return;
+            Stack<Point> stack = new Stack<Point>();
+            stack.Push(new Point(x, y));
+            while (stack.Count > 0)
+            {
+                Point p = stack.Pop();
+                if (img.GetPixel(p.X, p.Y) == targetColor)
+                {
+                    img.SetPixel(p.X, p.Y, col);
+                    if (p.X > 0)
+                        stack.Push(new Point(p.X - 1, p.Y));
+                    if (p.X < img.Width - 1)
+                        stack.Push(new Point(p.X + 1, p.Y));
+                    if (p.Y > 0)
+                        stack.Push(new Point(p.X, p.Y - 1));
+                    if (p.Y < img.Height - 1)
+                        stack.Push(new Point(p.X, p.Y + 1));
+                }
+            }
+            pictureBox1.Image = img;
+        }
+
+        private void LinearFloodFill(int x, int y)
+        {
+            Color targetColor = Color.FromArgb(0, 0, 0, 0);
+            if (targetColor == col)
+                return;
+            Stack<Point> stack = new Stack<Point>();
+            stack.Push(new Point(x, y));
+
+            while (stack.Count > 0)
+            {
+                Point p = stack.Pop();
+
+                if (img.GetPixel(p.X, p.Y) != targetColor)
+                    continue;
+
+                int leftBorder = p.X;
+                int rightBorder = p.X;
+
+                for (int i = p.X; i < img.Width; i++)
+                {
+                    if (img.GetPixel(i, p.Y) != targetColor)
+                    {
+                        break;
+                    }
+
+                    rightBorder = i;
+
+                    img.SetPixel(i, p.Y, col);
+                }
+
+                for (int i = p.X - 1; i >= 0; i--)
+                {
+                    if (img.GetPixel(i, p.Y) != targetColor)
+                    {
+                        break;
+                    }
+
+                    leftBorder = i;
+
+                    img.SetPixel(i, p.Y, col);
+                }
+
+                for (int i = leftBorder; i <= rightBorder; i++)
+                {
+                    if (p.Y + 1 < img.Height)
+                        stack.Push(new Point(i, p.Y + 1));
+                }
+
+                for (int i = leftBorder; i <= rightBorder; i++)
+                {
+                    if (p.Y - 1 > 0)
+                        stack.Push(new Point(i, p.Y - 1));
+                }
+
+            }
+                pictureBox1.Image = img;
+        }
+        private void LinearFloodFillWithImage(int x, int y)
+        {
+            Color targetColor = Color.FromArgb(0, 0, 0, 0);
+            /*if (targetColor == col)
+                return;*/
+            Stack<Point> stack = new Stack<Point>();
+            stack.Push(new Point(x, y));
+
+            while (stack.Count > 0)
+            {
+                Point p = stack.Pop();
+
+                if (img.GetPixel(p.X, p.Y) != targetColor)
+                    continue;
+
+                int leftBorder = p.X;
+                int rightBorder = p.X;
+
+                for (int i = p.X; i < img.Width; i++)
+                {
+                    if (img.GetPixel(i, p.Y) != targetColor)
+                    {
+                        break;
+                    }
+
+                    rightBorder = i;
+
+                    img.SetPixel(i, p.Y, tempImg.GetPixel(i%tempImg.Width,p.Y%tempImg.Height));
+                }
+
+                for (int i = p.X - 1; i >= 0; i--)
+                {
+                    if (img.GetPixel(i, p.Y) != targetColor)
+                    {
+                        break;
+                    }
+
+                    leftBorder = i;
+
+                    img.SetPixel(i, p.Y, tempImg.GetPixel(i % tempImg.Width, p.Y % tempImg.Height));
+                }
+
+                for (int i = leftBorder; i <= rightBorder; i++)
+                {
+                    if (p.Y + 1 < img.Height)
+                        stack.Push(new Point(i, p.Y + 1));
+                }
+
+                for (int i = leftBorder; i <= rightBorder; i++)
+                {
+                    if (p.Y - 1 > 0)
+                        stack.Push(new Point(i, p.Y - 1));
+                }
+
+            }
+            pictureBox1.Image = img;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                col = colorDialog.Color;
+                pen = new Pen(col, 5);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+                dlg.Filter = "bmp files (*.bmp)|*.bmp";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    tempImg = new Bitmap(dlg.FileName);
+                }
+            }
+        }
     }
 }
