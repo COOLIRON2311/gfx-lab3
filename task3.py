08ec938658609bb54bc49639b810c630d961b0fd
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2022-09-24 08:50:12 +0300

    Added points and color chooser

--- task3.py
+++ task3.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from tkinter import colorchooser
 
 Point = tuple[int, int]
+TkColor = str
 
 DIM = 500
 
@@ -9,6 +11,9 @@ class App(tk.Tk):
     r1: Point
     r2: Point
     r3: Point
+    c1: TkColor = 'red'
+    c2: TkColor = 'green'
+    c3: TkColor = 'blue'
     R: int = 10 # radius
 
     def __init__(self):
@@ -22,13 +27,14 @@ class App(tk.Tk):
         self.r3 = (100, 300)
         self.canvas.pack()
         self.canvas.bind("<B1-Motion>", self.move_point)
+        self.canvas.bind("<Button-3>", self.choose_color)
         self.draw_points()
         self.mainloop()
 
     def draw_points(self):
-        self.canvas.create_oval(self.r1[0], self.r1[1], self.r1[0] + self.R, self.r1[1] + self.R, fill="red")
-        self.canvas.create_oval(self.r2[0], self.r2[1], self.r2[0] + self.R, self.r2[1] + self.R, fill="green")
-        self.canvas.create_oval(self.r3[0], self.r3[1], self.r3[0] + self.R, self.r3[1] + self.R, fill="blue")
+        self.canvas.create_oval(self.r1[0], self.r1[1], self.r1[0] + self.R, self.r1[1] + self.R, fill=self.c1)
+        self.canvas.create_oval(self.r2[0], self.r2[1], self.r2[0] + self.R, self.r2[1] + self.R, fill=self.c2)
+        self.canvas.create_oval(self.r3[0], self.r3[1], self.r3[0] + self.R, self.r3[1] + self.R, fill=self.c3)
 
     def in_point(self, p: Point, x: int, y: int) -> bool:
         return (x - p[0]) ** 2 + (y - p[1]) ** 2 <= self.R ** 2
@@ -40,16 +46,20 @@ class App(tk.Tk):
             self.r2 = (event.x, event.y)
         elif self.in_point(self.r3, event.x, event.y):
             self.r3 = (event.x, event.y)
-        # if event.x < DIM / 3:
-        #     self.r1 = (event.x, event.y)
-        # elif event.x < DIM / 3 * 2:
-        #     self.r2 = (event.x, event.y)
-        # else:
-        #     self.r3 = (event.x, event.y)
+
         self.canvas.delete("all")
         self.draw_points()
 
+    def choose_color(self, event: tk.Event):
+        if self.in_point(self.r1, event.x, event.y):
+            self.c1 = colorchooser.askcolor()[1]
+        elif self.in_point(self.r2, event.x, event.y):
+            self.c2 = colorchooser.askcolor()[1]
+        elif self.in_point(self.r3, event.x, event.y):
+            self.c3 = colorchooser.askcolor()[1]
 
+        self.canvas.delete("all")
+        self.draw_points()
 
 if __name__ == "__main__":
     App()
